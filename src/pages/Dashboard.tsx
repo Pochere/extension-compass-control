34f99f466d57362b963d035b93f555878630ce8f
Pochere <pochere2008@gmail.com>
2025-04-16 21:54:31 +0300

    my-first-compass-control project

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+// src/pages/Dashboard.tsx
+const Dashboard = () => {
+    return <div>Dashboard Page</div>;
+  };
+  
+  // Add this line ▼
+  export default Dashboard;
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,7 @@
+// src/pages/Profile.tsx
+const Profile = () => {
+    return <div>Profile Page</div>;
+  };
+  
+  // Add this line ▼
+  export default Profile;
